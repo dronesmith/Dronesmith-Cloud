a2539031500f331285d9d559bb4744e00ea87ea6
flatlevel <geoff@skyworksas.com>
2015-06-19 23:13:49 -0700

    Cloudbit integration.

--- public/mods/Blocks/blocksPage.js
+++ public/mods/Blocks/blocksPage.js
@@ -1,6 +1,7 @@
 (function() {
     'use strict';
-	forgeApp.controller('BlocksCtrl', ['$scope', function($scope){
+	forgeApp.controller('BlocksCtrl', ['$scope', '$interval', 'Cloudbit',
+    function($scope, $interval, Cloudbit){
 			var Blocks = [
                 {"name" : "Block 1",
                  "text" : "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including ve"},
@@ -10,6 +11,38 @@
                  "text" : "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s"},
                  ];
 
+     $scope.updateCloudBit = function() {
+       Cloudbit
+        .get()
+        .$promise
+        .then(function(data) {
+          $scope.cloudbitModel = data;
+          $scope.updateValue = data.subscribers[0].data.input.percent.$cgte;
+        });
+     };
+
+     $scope.cloudbitParams = {
+       percent: 50,
+       timeToLive: 5000
+     };
+
+     $scope.postCloudBit = function() {
+
+       var cb = new Cloudbit($scope.cloudbitParams);
+       cb
+         .$save()
+         .then(function(data) {
+           if (data && data.success) {
+             console.log('yay!');
+           } else {
+             console.log('no :(');
+           }
+         })
+       ;
+     };
+
+
+     $interval($scope.updateCloudBit, 5000);
 
 
      $scope.list1 = {title: 'AngularJS - Drag Me'};
