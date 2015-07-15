ea9d7c84de280423d61b6b72c438dc70534f15a8
flatlevel <geoff@skyworksas.com>
2015-07-15 13:31:38 -0700

    Frontend beautification.

--- app.js
+++ app.js
@@ -79,6 +79,66 @@ app.use(lessMiddleware(path.join(__dirname, 'theme'), {
   debug: false
 }));
 
+var ig = require('instagram-node').instagram();
+
+/*
+
+OBTAINING AN ACCESS TOKEN
+
+(Note that they expire after a few days)
+
+1. Navigate to this URL:
+https://api.instagram.com/oauth/authorize/?client_id=5a013ce9cb2e4803b7611fe9da07409a&redirect_uri=http://stage.dronesmith.io&response_type=code&scope=likes+comments
+
+2. Take the code that was received and put it into this curl:
+(5bdbd1e452934a7ba9c4bfb7c75f2900)
+
+curl -F 'client_id=5a013ce9cb2e4803b7611fe9da07409a' \
+  -F 'client_secret=58d6f073de8e4219b97fa00a7299703d' \
+  -F 'grant_type=authorization_code' \
+  -F 'redirect_uri=http://stage.dronesmith.io' \
+  -F 'code=5bdbd1e452934a7ba9c4bfb7c75f2900' \
+  https://api.instagram.com/oauth/access_token
+
+3. You now have the access token.
+
+*/
+
+// Every call to `ig.use()` overrides the `client_id/client_secret`
+// or `access_token` previously entered if they exist.
+//
+//5a013ce9cb2e4803b7611fe9da07409a
+//CLIENT SECRET	58d6f073de8e4219b97fa00a7299703d
+//
+ig.use({ access_token: '1913424739.5a013ce.37962c6c31cd41e7985bda124536041d' });
+ig.use({ client_id: '5a013ce9cb2e4803b7611fe9da07409a',
+         client_secret: '58d6f073de8e4219b97fa00a7299703d' });
+
+/*
+curl -F 'client_id=5a013ce9cb2e4803b7611fe9da07409a' \
+  -F 'client_secret=58d6f073de8e4219b97fa00a7299703d' \
+  -F 'grant_type=authorization_code' \
+  -F 'redirect_uri=http://stage.dronesmith.io' \
+  -F 'code=9d7c92bbbfcc4630ad909ac92cc91316' \
+  https://api.instagram.com/oauth/access_token
+*/
+
+/*
+curl -F 'client_id=5a013ce9cb2e4803b7611fe9da07409a' \
+     -F 'client_secret=58d6f073de8e4219b97fa00a7299703d' \
+     -F 'object=tag' \
+     -F 'aspect=media' \
+     -F 'object_id=nofilter' \
+     -F 'callback_url=localhost/tags/droneparty' \
+     https://api.instagram.com/v1/subscriptions/
+*/
+
+// ig.add_tag_subscription('droneparty', 'http://stage.dronesmith.io/', function(err, result, remaining, limit){
+//   console.log(err);
+//   console.log(result);
+// });
+
+
 // Check for session, later authorization
 app.use(function (req, res, next) {
   if (!req.session) {
@@ -105,6 +165,29 @@ app.use(function (req, res, next) {
 // Init the router
 var Router = express.Router();
 var router = require('./routes/index')(app, Router);
+
+var mediaData ;
+
+/*
+curl -F 'access_token=1913424739.5a013ce.37962c6c31cd41e7985bda124536041d' \
+    -F 'text=This+is+my+comment' \
+    https://api.instagram.com/v1/media/1023274892831363799_1913424739/comments
+*/
+
+app.get('/tags/:name', function(req, res) {
+  ig.tag_media_recent(req.params.name, function(err, medias, pagination, remaining, limit) {
+    mediaData = medias;
+
+    ig.add_comment(mediaData[0].id, 'Awesome pic, ' + mediaData[0].user.username + '!',
+      function(err, result, remaining, limit) {
+        res
+          .status(200)
+          .json({result: err || result});
+    });
+  });
+
+
+});
 
 app.use('/', router);
 
