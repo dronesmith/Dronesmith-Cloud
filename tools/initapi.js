5d01fd12c93f42b0f47a687c19e2b18b85842385
flatlevel <geoff@skyworksas.com>
2015-11-06 11:53:09 -0800

    Redesigned the initapi script to make it aysnchronous friendly. Should fix the email issues.

--- tools/initapi.js
+++ tools/initapi.js
@@ -4,7 +4,8 @@
 var
   config = require('../config/config'),
   mongoose = require('mongoose'),
-  fs = require('fs')
+  fs = require('fs'),
+  async = require('async')
 ;
 
 var uri = 'mongodb://';
@@ -19,6 +20,15 @@ mongoose.connection.on('error', function(err) {
 });
 
 mongoose.connection.on('connected', function() {
+  // register model objects
+  var modelsDir = '../lib/models';
+  fs.readdirSync(modelsDir).forEach(function (file) {
+      if (/\.js$/.test(file)) {
+          // log.debug('requiring model file: ' + file);
+          require(modelsDir + '/' + file);
+      }
+  });
+
   var
     path = require('path'),
     User = mongoose.model('User'),
@@ -33,8 +43,9 @@ mongoose.connection.on('connected', function() {
     console.log("initapi - Adds API keys, creates user accounts, and sends an email.");
     console.log("\t--list\tA JSON list of emails to begin the process.");
     console.log("\t--id\t An id of the default drone to add for each user.");
+    console.log("\t--update\t Indicate the models should be updated, not removed.");
   } else {
-    var listfile = '', id = DRONE_MONGO_ID;
+    var listfile = '', id = DRONE_MONGO_ID, isUpdate = false;
 
     for (var k in process.argv) {
       var arg = process.argv[k];
@@ -46,6 +57,9 @@ mongoose.connection.on('connected', function() {
         case '--id':
           id = process.argv[+k+1] || DRONE_MONGO_ID;
           break;
+        case '--update':
+          isUpdate = true;
+          break;
       }
     }
 
@@ -56,54 +70,95 @@ mongoose.connection.on('connected', function() {
       process.exit(1);
     }
 
-    for (var i = 0; i < emails.length; ++i) {
-      var newPassword = Array(10).join((Math.random().toString(36)+'00000000000000000').slice(2, 18)).slice(0, 9);
-      var user = new User({
-        email:                  emails[i].email,
-        password:               newPassword,
-        confirmPassword:        newPassword,
-        fullName:               emails[i].name,
-        company:                emails[i].company,
-        kind:                   "other",
-        Otherkind:              "elevated",
-        apiKey:                 uuid.v4(),
-        drones:                 [ utils.castDocumentId(id) ]
-      });
-
-      user.setPassword(newPassword);
-
-      // tracking info
-      user.ipAddr = "lo";
-      user.referringUrl = null;
-      user.userAgent = "LFS";
-      user.onWaitList = false;
-      user.isVerified = true;
-
-      user.save(function(err) {
-        if (err) {
-          console.log(err);  
-        }
-      });
-      nodemailer.createTransport().sendMail({
-        from:     "hello@dronesmith.io",
-        to:       user.email,
-        subject:  "Your Forge Cloud Access Information",
-        html:     "<h2>Welcome to the next generation of Development.</h2>"
-                        + "<p>We really appreciate you checking out our Early Access, and we hope you'll enjoy using Forge.</p>\n"
-                        + "<p>Below is your login credentials and developer API key. It is advised you change your password"
-                        + " once logged in.</p>\n\n"
-                        + "<p>Email: <strong>" + user.email + "</strong></p>"
-                        + "<p>Password: <strong>" + newPassword + "</strong></p>"
-                        + "<p>API Key: <strong>" + user.apiKey + "</strong></p>",
-      })
+    async.each(emails, function(email, cb) {
+      var
+        newPassword = Array(10).join((Math.random().toString(36)+'00000000000000000').slice(2, 18)).slice(0, 9),
+        callbackList = [],
+        user
       ;
-    }
-  }
 
-  // mongoose.connection.close(function () {
-  //     console.log('DB connection terminated.');
-  //     process.exit(0);
-  // });
+      if (!isUpdate) {
+        callbackList.push(
+          function(cb) {
+            user = new User({
+              email:                  email.email,
+              password:               newPassword,
+              confirmPassword:        newPassword,
+              fullName:               email.name,
+              company:                email.company,
+              kind:                   "other",
+              Otherkind:              "elevated",
+              apiKey:                 uuid.v4(),
+              drones:                 [ utils.castDocumentId(id) ]
+            });
+
+            cb (null, user);
+          }
+        );
+      } else {
+        callbackList.push(
+          function(cb) {
+            User
+              .findOne({email: email.email})
+              .exec(function(err, user) {
+                return cb(err, user);
+              })
+            ;
+          }
+        );
+      }
+
+      callbackList.push(function(user, cb) {
+        user.setPassword(newPassword);
+        user.fullName = email.name;
+        user.company = email.company;
+        user.apiKey = uuid.v4();
+        user.drones =  [ utils.castDocumentId(id) ];
+
+        // tracking info
+        user.ipAddr = "lo";
+        user.referringUrl = null;
+        user.userAgent = "LFS";
+        user.onWaitList = false;
+        user.isVerified = true;
+        user.save(function(err) { cb(err, user); });
+      });
+
+      callbackList.push(function(user, cb) {
+        nodemailer.createTransport().sendMail({
+          from:     "hello@dronesmith.io",
+          to:       user.email,
+          subject:  "Your Forge Cloud Access Information",
+          html:     "<h2>Welcome to the next generation of Development.</h2>"
+                          + "<p>We really appreciate you checking out our Early Access, and we hope you'll enjoy using Forge.</p>\n"
+                          + "<p>Below is your login credentials and developer API key. It is advised you change your password"
+                          + " once logged in.</p>\n\n"
+                          + "<p>Email: <strong>" + user.email + "</strong></p>"
+                          + "<p>Password: <strong>" + newPassword + "</strong></p>"
+                          + "<p>API Key: <strong>" + user.apiKey + "</strong></p>",
+        }, function(err, info) {
+          console.log(err, info);
+          return cb(err);
+        })
+        ;
+      });
+
+      async.waterfall(callbackList, function(err, result) {
+        cb(err, result);
+      });
+
+    }, function(err, result) {
+      if (err) {
+        console.log(err);
+      }
+
+      mongoose.connection.close(function () {
+          console.log('DB connection terminated.');
+          process.exit(0);
+      });
+    });
+
+  }
 });
 
 process.on('SIGINT', function() {
@@ -111,13 +166,4 @@ process.on('SIGINT', function() {
         console.log('DB connection terminated.');
         process.exit(0);
     });
-});
-
-// register model objects
-var modelsDir = '../lib/models';
-fs.readdirSync(modelsDir).forEach(function (file) {
-    if (/\.js$/.test(file)) {
-        // log.debug('requiring model file: ' + file);
-        require(modelsDir + '/' + file);
-    }
 });
