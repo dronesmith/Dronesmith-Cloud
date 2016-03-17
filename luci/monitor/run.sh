80be35955e37057391b520de8e4e3e0f7c76bcc9
flatlevel <geoff@skyworksas.com>
2016-03-17 13:24:47 -0700

    Fix some typos in the mavproxy init

--- luci/monitor/run.sh
+++ luci/monitor/run.sh
@@ -2,6 +2,6 @@
 
 set -e
 
-mavproxy.py --master=/dev/ttyMFD1 --baud=921600 --daemon --out="module unload rc; output 0.0.0.0:14550; utput 0.0.0.0:14551;" &
+mavproxy.py --master=/dev/ttyMFD1 --baud=921600 --daemon --cmd="module unload rc; output add 0.0.0.0:14550; output add 0.0.0.0:14551;" &
 sleep 5
 node /opt/dss/luci/monitor app.js &
