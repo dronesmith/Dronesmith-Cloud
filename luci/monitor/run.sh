6859a66348dab59fea2853258f7039fd74cb8c37
flatlevel <geoff@skyworksas.com>
2016-04-07 18:06:14 -0700

    Add lucikit submodule

--- luci/monitor/run.sh
+++ luci/monitor/run.sh
@@ -2,6 +2,17 @@
 
 set -e
 
+ping -c 5 8.8.8.8>>/dev/null
+
+if [ $? -eq  0 ]
+  then
+  echo "Able to reach internet"
+  configure_edison --disableOneTimeSetup
+else
+  echo "Unable to reach internet"
+  configure_edison --enableOneTimeSetup
+fi
+
 mavproxy.py --master=/dev/ttyMFD1 --baud=921600 --daemon --cmd="module unload rc; output add 0.0.0.0:14550; output add 0.0.0.0:14551;" &
 sleep 5
 cd /opt/dss/luci/monitor
