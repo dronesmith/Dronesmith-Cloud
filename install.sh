a5dc70268be60eb3267d56222b594237a1ee940a
flatlevel <geoff@skyworksas.com>
2016-10-28 16:28:20 -0400

    Ops: Fix out of date packages, make cloud private package, fix install.sh

--- install.sh
+++ install.sh
@@ -5,7 +5,7 @@ set -e
 git submodule update --init --recursive
 git submodule foreach --recursive git submodule update --init
 npm install
-cd forg-ux/
+cd forge-ux/
 bower install
 cd ..
 mkdir log
